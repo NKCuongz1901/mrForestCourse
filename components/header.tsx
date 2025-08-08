56325e0722877a32f3d1c7d2a274cd1651eb22e1
nkhaccuong <cuong19012002@gmail.com>
2025-08-09 00:44:36 +0700

    Finish header

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,93 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import {
+    Navbar,
+    NavBody,
+    NavItems,
+    MobileNav,
+    NavbarLogo,
+    NavbarButton,
+    MobileNavHeader,
+    MobileNavToggle,
+    MobileNavMenu,
+  } from "@/components/ui/resizable-navbar";
+import { ModeToggle } from './ui/modeToggle';
 
 function Header() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const link = [
+        {
+            name: "Khóa Học",
+            link: '#course'
+        },
+        {
+            name: "Tư Vấn",
+            link: '#consult'
+        },
+        {
+            name: "Liên Hệ",
+            link: '#contact'
+        },
+    ]
   return (
-    <div>Header</div>
+    <header className='relative  w-full '>
+         <Navbar>
+        {/* Desktop Navigation */}
+        <NavBody>
+          <NavbarLogo />
+          <NavItems items={link} />
+          <div className="flex items-center gap-4">
+            <NavbarButton variant="primary">Book a call</NavbarButton>
+            <NavbarButton variant="secondary">
+                <ModeToggle/>
+            </NavbarButton>
+          </div>
+        </NavBody>
+ 
+        {/* Mobile Navigation */}
+        <MobileNav>
+          <MobileNavHeader>
+            <NavbarLogo />
+            <MobileNavToggle
+              isOpen={isMobileMenuOpen}
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            />
+          </MobileNavHeader>
+ 
+          <MobileNavMenu
+            isOpen={isMobileMenuOpen}
+            onClose={() => setIsMobileMenuOpen(false)}
+          >
+            {link.map((item, idx) => (
+              <a
+                key={`mobile-link-${idx}`}
+                href={item.link}
+                onClick={() => setIsMobileMenuOpen(false)}
+                className="relative text-neutral-600 dark:text-neutral-300"
+              >
+                <span className="block">{item.name}</span>
+              </a>
+            ))}
+            <div className="flex w-full flex-col gap-4">
+              <NavbarButton
+                onClick={() => setIsMobileMenuOpen(false)}
+                variant="primary"
+                className="w-full"
+              >
+                Login
+              </NavbarButton>
+              <NavbarButton
+                onClick={() => setIsMobileMenuOpen(false)}
+                variant="primary"
+                className="w-full"
+              >
+                Book a call
+              </NavbarButton>
+            </div>
+          </MobileNavMenu>
+        </MobileNav>
+      </Navbar>
+    </header>
   )
 }
 
